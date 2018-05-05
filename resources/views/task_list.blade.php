0bbec47621a9483e6bafb9176b3d08cc1ce756eb
gloverac08 <glover.amy@gmail.com>
2018-05-05 15:18:22 -0400

    Add basic html for task list page

--- resources/views/task_list.blade.php
+++ resources/views/task_list.blade.php
@@ -1,6 +1,16 @@
 @extends('layout')
 
 @section('content')
-Task List Page
+    <div class="container">
+        <H1>Tasks</H1>
+        <div>
+            <input id="textInput" class="custom" size="32"/>
+            <button>Add new task</button>
+        </div>
+        <ul class="list-group">
+            <li class="list-group-item"><button>Delete</button>First task</li>
+        </ul>
+    </div>
+
 @endsection
 
