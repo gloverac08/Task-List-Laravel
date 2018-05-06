10e6d0f2a830664251b6ec177b7e635d8e9b4eb0
gloverac08 <glover.amy@gmail.com>
2018-05-06 18:30:33 -0400

    Add some of the default styling back in
    Also routes landing page to tasks page

--- resources/views/task_list.blade.php
+++ resources/views/task_list.blade.php
@@ -1,19 +1,18 @@
 @extends('layout')
 
 @section('content')
-    <div class="container-fluid heading">
-        <H1 class="title">Task List</H1>
-    </div>
-    <div class="container">
-    
-        <div class="card new-task">
-        <!-- Display Validation Errors -->
-        {{-- @include('common.errors') --}}
-        <!-- New Task Form -->
-            <div class="card-header">
-                <h4>Add a new task</h4>
-            </div>
-            <div class="card-body">
+<div class="container-fluid heading">
+    <H1 class="title">Task List</H1>
+</div>
+<div class="container">
+    <div class="card new-task">
+    <!-- Display Validation Errors -->
+    {{-- @include('common.errors') --}}
+    <!-- New Task Form -->
+        <div class="card-header">
+            <h4>Add a new task</h4>
+        </div>
+        <div class="card-body">
             <form action="/create" method="POST" class="form-horizontal col-6">
                 @csrf
                 <!-- Task Name -->
@@ -22,63 +21,55 @@
                         <input type="text" name="body" id="task-body" class="form-control">
                     </div>
                 </div>
-
                 <!-- Add Task Button -->
                 <div class="form-group">
-                <div class="col-sm-offset-3 col-sm-6">
-                    <button type="submit" class="btn btn-secondary">
-                    <i class="fa fa-plus"></i> Add Task
+                    <div class="col-sm-offset-3 col-sm-6">
+                        <button type="submit" class="btn btn-secondary">
+                            <i class="fa fa-plus"></i> Add Task
                         </button>
-                </div>
+                    </div>
                 </div>
             </form>
         </div>
-        </div>
-        <!-- Display all tasks -->
-        <div class="card tasks">
-            <div class="card-header">
-               <h3>Tasks</h3>
-            </div>
-        <table class="table">
-            <tbody>
-                @if(count($tasks) > 0)
-                @foreach($tasks as $task)
-                    <tr class="table-text"> 
-                        <td class="col-6">
-                            <h5>{{$task->body}}</h5>
-                        </td>
-                        <td class="col-1">
-                            <form action="/tasks/{{$task->id}}" method="POST">
-                                @method('DELETE')
-                                @csrf
-                                <button type="submit" class="btn btn-danger">Delete</button>
-                            </form>
-                        </td>
-                        
-                        <td class="edit col-1">@include('edit_modal', ['text' => $task->body, 'id' => $task->id])</td>
-                    
-                        <!-- Delete button -->
-                        {{-- <td>
-                            
-                        </td>
-                        <td>
-                            
-                        </td> --}}
-                        
-                    </tr>
-                @endforeach
-                @else
-                    <tr>
-                        <td>
-                            <div>
-                                No tasks saved yet. Add a new task
-                            </div>
-                        </td>
-                    </tr>
-                @endif
-            </tbody>
-        </div>
- 
     </div>
+    <!-- Display all tasks -->
+    <div class="card tasks">
+        <div class="card-header">
+            <h3>Tasks</h3>
+        </div>
+    <table class="table">
+        <tbody>
+            @if(count($tasks) > 0)
+            @foreach($tasks as $task)
+                <tr class="table-text"> 
+                    <td class="col-6">
+                        <h5>{{$task->body}}</h5>
+                    </td>
+                    <!-- Delete button -->
+                    <td class="col-1">
+                        <form action="/tasks/{{$task->id}}" method="POST">
+                            @method('DELETE')
+                            @csrf
+                            <button type="submit" class="btn btn-danger">Delete</button>
+                        </form>
+                    </td>
+                        <!-- Edit button/modal -->
+                    <td class="edit col-1">
+                        @include('edit_modal', ['text' => $task->body, 'id' => $task->id])
+                    </td>
+                </tr>
+            @endforeach
+            @else
+                <tr>
+                    <td>
+                        <div>
+                            No tasks saved yet. Add a new task
+                        </div>
+                    </td>
+                </tr>
+            @endif
+        </tbody>
+    </div>
+</div>
 @endsection
 
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -12,18 +12,17 @@
             @endauth
         </div>
     @endif
-
+    
     <div class="content">
         <div class="title m-b-md">
-            Laravel
+            Task List
         </div>
-
         <div class="links">
-            <a href="https://laravel.com/docs">Documentation</a>
-            <a href="https://laracasts.com">Laracasts</a>
-            <a href="https://laravel-news.com">News</a>
-            <a href="https://forge.laravel.com">Forge</a>
-            <a href="https://github.com/laravel/laravel">GitHub</a>
+             <!-- Routes to Tasks Page -->
+            <form action="/tasks" method="GET">
+                @csrf
+                <button class="btn btn-secondary btn-lg">Enter</button>
+            </form>
         </div>
     </div>
 </div>
